723548c038e036a24c45a60f5a7fb35fde1d8428
Valery <valera071998@gmail.com>
2019-11-07 14:49:49 +0600

    feat(canvas): set canvas view as content view of activity

--- app/src/main/java/dev/valeryvpetrov/minipaint/MainActivity.kt
+++ app/src/main/java/dev/valeryvpetrov/minipaint/MainActivity.kt
@@ -2,11 +2,16 @@ package dev.valeryvpetrov.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val canvasView = CanvasView(this)
+        canvasView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        canvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(canvasView)
     }
 }
