dcb21865a5b5a904538044d73c4bf855133c2fbf
Valery <valera071998@gmail.com>
2019-11-07 15:02:08 +0600

    feat(canvas): setup canvas caching

--- app/src/main/java/dev/valeryvpetrov/minipaint/CanvasView.kt
+++ app/src/main/java/dev/valeryvpetrov/minipaint/CanvasView.kt
@@ -1,8 +1,27 @@
 package dev.valeryvpetrov.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class CanvasView(context: Context) : View(context) {
 
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat
+        .getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()  // free up memory
+
+        extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)    // create new
+        extraCanvas = Canvas(extraBitmap)
+
+        extraCanvas.drawColor(backgroundColor)  // fill background
+    }
 }
